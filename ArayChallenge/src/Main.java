0c3364826fbab560e77c38e145c335f85fae0e37
Darwin Jacob <72287071+jacob-fry85@users.noreply.github.com>
2020-10-20 21:21:40 +0700

    Deleting unuse comments and variable
    Commit

--- ArayChallenge/src/Main.java
+++ ArayChallenge/src/Main.java
@@ -21,24 +21,14 @@ public class Main {
     }
 
     public static int[] sortIntegers(int[] array) {
-        int number = 0;
-        boolean flag = false;
         int temp=0;
         for (int i = 0; i < array.length; i++) {
-            number = array[i];
-            //System.out.println("Outside i number = " +  number + " i ke " + i + " array["+ i +"] = " +  array[i]);
             for(int j = i + 1 ; j<= array.length - 1;j++) {
-                if(number < array[j]) {
-                    //System.out.println("Inside j number = " + number + " i array["+ i +"] = " + array[i] + " j array["+j+"] = " + array[j]);
+                if(array[i] < array[j]) {
                     temp = array[i];
                     array[i] = array[j];
                     array[j] = temp;
                 }
-               /* System.out.print("hasil sort : ");
-                for(int k : array) {
-                    System.out.print(k + " ");
-                }
-                System.out.println(); */
             }
         }
         return array;
